6023f411b4a303cba8bb63a37efbf893e1b93521
Manali <56449889+ManaliAundhkar@users.noreply.github.com>
2020-03-29 14:31:41 +0530

    Update Demo3.java

--- Demo3.java
+++ Demo3.java
@@ -1,7 +1,7 @@
 //Write Java program which accept String from user and display below pattern.
 //Input : Hello
 //Output : H e l l o
-//		   H e l l
+//	   H e l l
 //         H e l
 //         H e
 //         H
